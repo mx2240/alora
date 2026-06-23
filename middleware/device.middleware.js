525d9c2042cf9a78d5dd1e4beed0d0dfe1584fd5
mx2240 <lovemicky10@gmail.com>
2026-06-23 10:35:38 +0000

    up

--- middleware/device.middleware.js
+++ middleware/device.middleware.js
@@ -1,12 +1,4 @@
-// module.exports = (req, res, next) => {
-//     const key = req.headers["x-device-key"];
 
-//     if (!key || key !== process.env.DEVICE_SECRET) {
-//         return res.status(403).json({ error: "Unauthorized device" });
-//     }
-
-//     next();
-// };
 
 
 module.exports = (req, res, next) => {
