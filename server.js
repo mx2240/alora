99456109cfbfab2a957f4ccd0fbc20d71e0f1170
mx2240 <lovemicky10@gmail.com>
2026-06-23 04:15:13 +0000

    u

--- server.js
+++ server.js
@@ -1,52 +1,21 @@
-// const app = require("./app");
-
-// const PORT = 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`✅ Server running on http://localhost:${PORT}`);
-// });
 
 
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// const app = require("./app");
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log("");
-//     console.log("===================================");
-//     console.log("🚀 ALORA BACKEND V2");
-//     console.log("===================================");
-//     console.log(`Server: http://localhost:${PORT}`);
-//     console.log(`Health: http://localhost:${PORT}/health`);
-//     console.log("===================================");
-//     console.log("");
-// });
-
-
-
-// const app = require("./src/app");
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log("🔥 ALORA BACKEND V2 RUNNING");
-//     console.log("http://localhost:" + PORT);
-// });
-
-
-
-
-const app = require("./src/app");
+const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-        console.log(`Server running on ${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log("");
+    console.log("===================================");
+    console.log("🚀 ALORA BACKEND V2");
+    console.log("===================================");
+    console.log(`Server: http://localhost:${PORT}`);
+    console.log(`Health: http://localhost:${PORT}/health`);
+    console.log("===================================");
+    console.log("");
+});
 
-module.exports = app;
+
